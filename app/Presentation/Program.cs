0edfc69c40ee9f2bafa64d93778198662625a8e1
YaMiN <yamin8000@yahoo.com>
2023-11-07 18:29:35 +0330

    added JWT to swagger

--- app/Presentation/Program.cs
+++ app/Presentation/Program.cs
@@ -1,7 +1,8 @@
+using System.Reflection;
 using Infrastructure.Interfaces;
 using Infrastructure.Persistence;
 using Infrastructure.Repositories;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
 using UMS.Authentication.Application.Authorization;
 using UMS.Authentication.Application.Dtos;
 using UMS.Authentication.Application.Interfaces;
@@ -41,7 +42,41 @@ builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("JWT"))
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "Infrastructure.xml"));
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "UMS.Authentication.Application.xml"));
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "UMS.Authentication.Domain.xml"));
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description =
+            "JWT Authorization header using the Bearer scheme. Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
+});
 
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
