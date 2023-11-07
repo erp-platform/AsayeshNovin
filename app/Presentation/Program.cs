1f0f8487a0d0c03cd5c0a1f910612352b3fda707
Arian Tron <ariantron@yahoo.com>
2023-11-07 12:15:50 +0330

    Restore Swagger In Production Mode

--- app/Presentation/Program.cs
+++ app/Presentation/Program.cs
@@ -41,11 +41,11 @@ AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
+// {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+// }
 
 app.UseHttpsRedirection();
 
