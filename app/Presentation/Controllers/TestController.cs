98df3c94a8e625a167f05e2eacae5764a490efdb
Arian Tron <ariantron@yahoo.com>
2023-11-07 19:45:13 +0330

    Add TestController

---
+++ app/Presentation/Controllers/TestController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UMS.Authentication.Application.Dtos;
+using UMS.Authentication.Application.Interfaces;
+using UMS.Authentication.Domain.Entities;
+
+namespace Presentation.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+[Consumes("application/json")]
+[Produces("application/json")]
+public class TestController:ControllerBase
+{
+    private readonly IBaseService<User, UserCreateDto, UserUpdateDto> _userService;
+
+    public TestController(IBaseService<User, UserCreateDto, UserUpdateDto> userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpGet("Db")]
+    public virtual async Task<IActionResult> Db()
+    {
+        return Ok(await _userService.GetAllAsync());
+    }
+    
+    [HttpGet("Ping")]
+    public virtual Task<IActionResult> Ping()
+    {
+        return Task.FromResult<IActionResult>(Ok("Pong"));
+    }
+}
