6778181319d501ea7f7dfcb30acea08c481a5711
YaMiN <yamin8000@yahoo.com>
2023-11-07 19:05:03 +0330

    added some docs

--- app/Presentation/Controllers/BaseController.cs
+++ app/Presentation/Controllers/BaseController.cs
@@ -6,6 +6,12 @@ using UMS.Authentication.Application.Authorization;
 
 namespace Presentation.Controllers;
 
+/// <summary>
+/// Basic CRUD Controller
+/// </summary>
+/// <typeparam name="TEntity"></typeparam>
+/// <typeparam name="TCreateDto"></typeparam>
+/// <typeparam name="TUpdateDto"></typeparam>
 [Authorize]
 [ApiController]
 [Route("[controller]")]
@@ -24,42 +30,77 @@ public class BaseController<TEntity, TCreateDto, TUpdateDto>
         _service = service;
     }
 
+    /// <summary>
+    /// Get all items
+    /// </summary>
+    /// <returns></returns>
     [HttpGet]
     public virtual async Task<IActionResult> GetAllAsync()
     {
         return Ok(await _service.GetAllAsync());
     }
 
+    /// <summary>
+    /// Get by id
+    /// </summary>
+    /// <param name="id">Item's id</param>
+    /// <returns></returns>
     [HttpGet("{id:guid}")]
     public virtual async Task<IActionResult> GetByIdAsync(Guid id)
     {
         return Ok(await _service.FindByIdAsync(id));
     }
 
+    /// <summary>
+    /// Create new item
+    /// </summary>
+    /// <param name="createDto"></param>
+    /// <returns></returns>
     [HttpPost]
     public virtual async Task<IActionResult> CreateAsync([FromBody] TCreateDto createDto)
     {
         return Ok(await _service.CreateAsync(createDto));
     }
 
+    /// <summary>
+    /// Update item 
+    /// </summary>
+    /// <param name="id">Item's id</param>
+    /// <param name="updateDto"></param>
+    /// <returns></returns>
     [HttpPut("{id:guid}")]
     public virtual async Task<IActionResult> UpdateAsync(Guid id, [FromBody] TUpdateDto updateDto)
     {
         return Ok(await _service.UpdateAsync(id, updateDto));
     }
 
+    /// <summary>
+    /// Soft delete item
+    /// </summary>
+    /// <param name="id">Item's id</param>
+    /// <returns></returns>
     [HttpPost("delete/{id:guid}")]
     public virtual async Task<IActionResult> Delete(Guid id)
     {
         return Ok(await _service.DeleteAsync(id));
     }
 
+    /// <summary>
+    /// Restore soft deleted item
+    /// </summary>
+    /// <param name="id">Item's id</param>
+    /// <returns></returns>
     [HttpPost("restore/{id:guid}")]
     public virtual async Task<IActionResult> Restore(Guid id)
     {
         return Ok(await _service.RestoreAsync(id));
     }
 
+    /// <summary>
+    /// Force delete item
+    /// </summary>
+    /// <param name="id">Item's id</param>
+    /// <returns></returns>
     [HttpDelete("{id:guid}")]
     public virtual async Task<IActionResult> ForceDeleteAsync(Guid id)
     {
