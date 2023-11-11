bc4d971a7ad0a3b22e78cb42dbd5091a40e16526
Yamin Siahmargooei <5001708+yamin8000@users.noreply.github.com>
2023-11-11 14:40:54 +0330

    Update AddressCreateDto.cs

--- services/UMS.Profile/UMS.Profile.Application/Dtos/AddressDtos/AddressCreateDto.cs
+++ services/UMS.Profile/UMS.Profile.Application/Dtos/AddressDtos/AddressCreateDto.cs
@@ -4,8 +4,8 @@ namespace UMS.Profile.Application.Dtos.AddressDtos;
 
 public class AddressCreateDto
 {
-    [Required] public required string Text { get; set; }
+    public required string Text { get; set; }
     public int? PostalCode { get; set; }
     public string? Phone { get; set; }
-    [Required] public required string City { get; set; }
+    public required string City { get; set; }
 }
