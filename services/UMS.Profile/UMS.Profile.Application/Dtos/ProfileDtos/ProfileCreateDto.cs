57bb7a5572dbfbfb333d8b852dcf62716c590d28
Yamin Siahmargooei <5001708+yamin8000@users.noreply.github.com>
2023-11-11 14:41:15 +0330

    Update ProfileCreateDto.cs

--- services/UMS.Profile/UMS.Profile.Application/Dtos/ProfileDtos/ProfileCreateDto.cs
+++ services/UMS.Profile/UMS.Profile.Application/Dtos/ProfileDtos/ProfileCreateDto.cs
@@ -4,8 +4,8 @@ namespace UMS.Profile.Application.Dtos.ProfileDtos;
 
 public class ProfileCreateDto
 {
-    [Required] public required string Forename { get; set; }
-    [Required] public required string Surname { get; set; }
+    public required string Forename { get; set; }
+    public required string Surname { get; set; }
     public string? MiddleName { get; set; }
     public string? NationalCode { get; set; }
     public DateTime? BirthDate { get; set; }
