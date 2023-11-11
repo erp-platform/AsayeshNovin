397caf1c3df5f270f9304ebcafb166de7116f5fe
Pouria <100228221+theOOwl@users.noreply.github.com>
2023-11-11 13:31:58 +0330

    Add Address and Profile Dtos for ProfileService

---
+++ services/UMS.Profile/UMS.Profile.Application/Dtos/AddressDtos/AddressCreateDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UMS.Profile.Application.Dtos.AddressDtos;
+
+public class AddressCreateDto
+{
+    [Required] public required string Text { get; set; }
+    public int? PostalCode { get; set; }
+    public string? Phone { get; set; }
+    [Required] public required string City { get; set; }
+}
---
+++ services/UMS.Profile/UMS.Profile.Application/Dtos/ProfileDtos/ProfileCreateDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UMS.Profile.Application.Dtos.ProfileDtos;
+
+public class ProfileCreateDto
+{
+    [Required] public required string Forename { get; set; }
+    [Required] public required string Surname { get; set; }
+    public string? MiddleName { get; set; }
+    public string? NationalCode { get; set; }
+    public DateTime? BirthDate { get; set; }
+    public string? FatherName { get; set; }
+}
