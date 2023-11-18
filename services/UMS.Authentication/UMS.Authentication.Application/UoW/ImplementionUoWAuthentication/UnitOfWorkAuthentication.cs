26f836573d7a25b1819db97d9e17226904f47b5e
Pouria <100228221+theOOwl@users.noreply.github.com>
2023-11-18 21:33:56 +0330

    add the body of UoW implemention

--- services/UMS.Authentication/UMS.Authentication.Application/UoW/ImplementionUoWAuthentication/UnitOfWorkAuthentication.cs
+++ services/UMS.Authentication/UMS.Authentication.Application/UoW/ImplementionUoWAuthentication/UnitOfWorkAuthentication.cs
@@ -1,30 +1,46 @@
-﻿using UMS.Authentication.Application.Interfaces;
+﻿using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using UMS.Authentication.Application.Interfaces;
 using UMS.Authentication.Application.UoW.InterfaceUoWAuthentication;
 
 namespace UMS.Authentication.Application.UoW.ImplementionUoWAuthentication;
 
 public class UnitOfWorkAuthentication : IUnitOfWorkAuthentication
 {
+    DbContext _context;
+    IDbContextTransaction _transaction;
+
+
+    public UnitOfWorkAuthentication(AppDbContext context)
+    {
+        _context = context;
+        _transaction = _context.Database.BeginTransaction();
+    }
+
     public void Dispose()
     {
-        throw new NotImplementedException();
-    }
+        Dispose(true);
+        GC.SuppressFinalize(this);    }
 
     public void Save()
     {
-        throw new NotImplementedException();
+        _context.SaveChanges();
     }
 
     public void Commit()
     {
-        throw new NotImplementedException();
+        _transaction.Commit();
     }
 
     public void Rollback()
     {
-        throw new NotImplementedException();
+        _transaction.Rollback();
     }
-
+    protected virtual void Dispose(bool disposing)
+    {
+        _context?.Dispose();
+    }
     public IAuthService AuthService()
     {
         throw new NotImplementedException();
